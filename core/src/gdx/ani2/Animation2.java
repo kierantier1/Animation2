d6add1653e7602fddc41ba6107f360f1aaa8f422
hallk1331 <hallk1331@FHC-IC0034079.wrdsb.ca>
2017-12-13 13:06:47 -0500

    Experimenting with Animation (still)

---
+++ core/src/gdx/ani2/Animation2.java
@@ -0,0 +1,88 @@
+package gdx.ani2;
+
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Animation2 implements InputProcessor {
+    SpriteBatch batch;
+    Texture img;
+	
+	
+    public void create () {
+		
+    }
+
+    public void render () {
+	Gdx.gl.glClearColor(1, 0, 0, 1);
+	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	batch.begin();
+	
+	batch.end();
+    }
+        
+    public void show() {
+    }
+
+    
+    public void resize(int width, int height) {
+    }
+
+   
+    public void pause() {
+    }
+
+    
+    public void resume() {
+    }
+
+    public void hide() {
+    }
+
+    public void dispose() {
+        img.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
